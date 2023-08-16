2abd7b9df4d344b234462ae998ac8294d57e06fc
Chukwukadibia <chukwukadiemma@gmail.com>
2023-08-16 15:56:05 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/104-fobonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long int fibonacci[98];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 98; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		printf(", %ld",fibonacci[i]);
+	}
+
+	return (0);
+}
