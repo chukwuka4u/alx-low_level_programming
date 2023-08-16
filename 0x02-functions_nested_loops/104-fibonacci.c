5589208a4938a78b4844f20598285f7a51bb7b64
Chukwukadibia <chukwukadiemma@gmail.com>
2023-08-16 16:06:41 -0700

    corrections

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,12 +13,15 @@ int main(void)
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
-	printf("%ld, %ld", fibonacci[0], fibonacci[1]);
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
 	for (i = 2; i < 98; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		printf(", %ld",fibonacci[i]);
+		if (i == 97)
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
 	}
 
 	return (0);
