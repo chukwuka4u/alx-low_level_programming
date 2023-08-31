de599a7f122956ba888ea56d8ae24d6730e99d6f
Chukwukadibia <chukwukadiemma@gmail.com>
2023-08-30 21:24:53 -0700

    advanced tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "main.h"
+int _strlen(char *s);
+int _helper(char *s, int len);
 
 /**
  * is_palindrome - function returns 1 if a string is a palindrome and 0 if not
@@ -7,5 +9,42 @@
  */
 int is_palindrome(char *s)
 {
-	
+	int len;
+
+	len = _strlen(s);
+	if (len <= 1)
+		return (1);
+	return (_helper(s, len));
+}
+
+/**
+ * _strlen - function to get the length of the string
+ * @s: An input string
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (_strlen(s + 1) + 1);
+}
+
+/**
+ * _helper -function to compare characters
+ * @len: length of the string
+ * @s: An input string
+ * Return: A reverse string
+ */
+int _helper(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
+	{
+		return (_helper(s + 1, len - 2));
+	}
+	else
+		return (0);
 }
