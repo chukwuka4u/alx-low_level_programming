8ecd369144d5427382ac0ae58f5741bd8fee8d8d
Chukwukadibia <chukwukadiemma@gmail.com>
2023-09-17 14:45:24 -0700

    function pointers in C

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - returns the sum of two integers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference btwn two integers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of two integers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of two integers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of two integers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
