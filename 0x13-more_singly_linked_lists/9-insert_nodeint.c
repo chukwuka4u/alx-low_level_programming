1f79b6cbdb0d1fec4b0dc9577b871a823f2f3cd7
Chukwukadibia <chukwukadiemma@gmail.com>
2023-09-27 14:30:33 -0700

    corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	if (*head == NULL)
 	{
 		*head = new_node;
