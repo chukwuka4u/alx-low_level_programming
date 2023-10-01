006d4ce039b69120bc3d3b9fcc7ef405a70ada47
Chukwukadibia <chukwukadiemma@gmail.com>
2023-09-30 21:13:55 -0700

    corrections

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * clear_bit - A function that sets a bit to 0 at given index
- * @n: The number to set bit in
- * @index: The index to set bit at
- * Return: 1 if it worked, or -1 on error
+ * flip_bits - A function that gets number of bits needed to flip
+ * @n: How many bit flips are needed to equal m for n
+ * @m: The number to set other equal
+ * Return: The number of fliped bits
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int flips = 0;
+	unsigned long int xor = (n ^ m);
 	unsigned long int max = 0x01;
 
-	max = ~(max << index);
-	if (max == 0x00)
-		return (-1);
-	*n &= max;
-	return (1);
+	while (max <= xor)
+	{
+		if (max & xor)
+			flips++;
+		max <<= 1;
+	}
+	return (flips);
 }
